eb5ed185fdc9e0b8d3b7846711b9411b0768a211
allen <allen.hong@ixiaoshuidi.com>
2017-02-15 16:40:20 +0800

    Initial Commit

---
+++ HMSparkButton/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HMSparkButton
+//
+//  Created by hongyu on 2017/2/15.
+//  Copyright © 2017年 Allen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HMSparkButton/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HMSparkButton
+//
+//  Created by hongyu on 2017/2/15.
+//  Copyright © 2017年 Allen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
